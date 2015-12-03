4b0d521bf2c5dbe5fcc9e73168e60c35557c9de1
Elektrospy <elektrospy@gmail.com>
2015-12-03 19:33:01 +0100

    added missing changes

--- GLaDOSServoControl.cpp
+++ GLaDOSServoControl.cpp
@@ -12,6 +12,9 @@ GLaDOSServoControl::GLaDOSServoControl(Servo newServo, uint16_t startPos, uint16
 	_rangeMin = 650;
 	_rangeMax = 2400;
 	_curPos = 1500;
+	// start and end speed
+	_startSpeed = 3;
+	_endSpeed = 3;
 
 	if (rangeMin >= _rangeMin && rangeMax <= _rangeMax) {
 		_rangeMin = rangeMin;
@@ -35,16 +38,14 @@ GLaDOSServoControl::GLaDOSServoControl(Servo newServo, uint16_t startPos, uint16
 	_speed = 0; //a value that consists of small increment values that change in magnitude depending on if the wave starts slow and ends fast or vise versa.
 	_speedTick = 0; //ticks off how long the hold position for the servo is in very small ms increments giving the illusion of a slower or faster moving servo
 	_amplitude = 0;   //a# amplitude higher value taller wave shorter value shorter wave by magnitude:   a=(highest # - lowest #)/2
-	_phaseShift = 0;   //b# lower value = longer wave or higher value=shorter wave this is phase shift or stretch of function b=2pi/(period*2) where period is desired wave size
+	//_phaseShift = 0;   //b# lower value = longer wave or higher value=shorter wave this is phase shift or stretch of function b=2pi/(period*2) where period is desired wave size
+	_phaseShift = (2 * _pie / (_sineSize * 2)); //coefficient of sine math function
 	_frequencyOffset = 0;   //c# is x frequency offset = what part of curve want to look at
 	_yOffset = 0;   //d# is y offset  = 0.5*amplitude shifts the curve so it is wholey in 1st quadrant
 	_atEnd = 1;   //trigger value either 0 or 1 to declare that that servo has reached its final position and so servo movement sequence of all servos (once all report per#=1)can end.
 	_exForCount = 0;
 	_numberOfSinusWaves = 1;
 	_msWaitTime = 10;
-	// start and end speed
-	_startSpeed = 5;
-	_endSpeed = 5;
 
 	//_thisServo.writeMicroseconds(_curPos);
 }
@@ -58,8 +59,7 @@ bool GLaDOSServoControl::setNewPos(uint16_t newPos) {
 			_sineWaveVar = 1;
 			_countExtended = 1;
 			_speedTick = 1;
-			_phaseShift = (2 * _pie / (_sineSize * 2)); //coefficient of sine math function
-			_sineSize = ((_numberOfSinusWaves * 2) - 1) * _sineSize; //ranges from _numberOfSinusWaves=1,2,3,4,5 _sineSize#= 1*1700,3*1700,5*1700,7*1700
+			//_sineSize = ((_numberOfSinusWaves * 2) - 1) * _sineSize; //ranges from _numberOfSinusWaves=1,2,3,4,5 _sineSize#= 1*1700,3*1700,5*1700,7*1700
 			_exForCount = 0;
 
 			//position dependent sine wave coeficients
@@ -118,20 +118,13 @@ bool GLaDOSServoControl::isAtEndPos(){return _atEnd;}
 void GLaDOSServoControl::nextStep() {
 	if(_exForCount < _eventCycle && _atEnd != 1)
 	{
-		//traditional speed values start off as spa# and end up as spb# as _exForCount# ticks away on the fly as curve is being drawn.
+		// traditional speed values start off as spa# and end up as spb# as _exForCount# ticks away on the fly as curve is being drawn.
 		// result is a sine curve that is compressed in the x axis on one end (spa#=large number) and stretched on other end (spb#=small number).
 		if (_startSpeed > _endSpeed) {_speed = ((_countExtended + 1) / _sineSize) * (_startSpeed - _endSpeed) + _endSpeed;} //start fast end slow
 		else {_speed = ((_countExtended + 1) / _sineSize) * (_endSpeed - _startSpeed) + _startSpeed;} // start slow end fast
 
-		// servo #1   3 states or cases
-		//if (_exForCount < _msWaitTime) //condition 1 servo not ready to move yet
-		//{
-			//_thisServo.writeMicroseconds(_prePos);
-		//}
-		//else 
 		if (_exForCount > _msWaitTime && _sineWaveVar > _sineSize) //condition 3 motion is done and position is held
 		{
-			//_thisServo.writeMicroseconds(_curPos);
 			_prePos = _newPos;
 			_newPos = _curPos;
 			_atEnd = 1; //declares this servo is finished with its movement
@@ -149,7 +142,6 @@ void GLaDOSServoControl::nextStep() {
 			//sine wave is sustained at old value for 1 to speed# as counted by speedtick#
 			else if (_speedTick > 1 && _speedTick < _speed)  
 			{
-				//_thisServo.writeMicroseconds(_prePos);
 				_speedTick += 1;  //increments _speedTick to delay the servo at one position along its travel to simulate a speed
 			}
 			//sine wave is now permitted to continue drawing and moving to points by having speedtick# reset
